db835bfb4802bcb4591545383cd429c2f7040189
portlek <hsndmrts_@outlook.com>
2019-11-10 01:23:50 +0300

    finish

---
+++ src/main/java/io/github/portlek/sign/Menu.java
@@ -0,0 +1,118 @@
+package io.github.portlek.sign;
+
+import io.github.portlek.itemstack.util.Colored;
+import io.github.portlek.itemstack.util.XMaterial;
+import io.github.portlek.nbt.api.NBTCompound;
+import io.github.portlek.nbt.base.EmptyNBTOf;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+public final class Menu {
+
+    private static final int SIGN_LINES = 4;
+
+    private static final String NBT_FORMAT = "{\"text\":\"%s\"}";
+
+    private static final String NBT_BLOCK_ID = "minecraft:sign";
+
+    private static final int ACTION_INDEX = 9;
+
+    private static final String OPEN_PACKET = "PacketPlayOutOpenSignEditor";
+
+    private static final String ENTITY_DATA_PACKET = "PacketPlayOutTileEntityData";
+
+    @NotNull
+    private final Player player;
+
+    @NotNull
+    private final List<String> text;
+
+    private BiPredicate<Player, List<String>> response;
+
+    private boolean reopenIfFail;
+
+    private Consumer<Location> onOpen;
+
+    public Menu(@NotNull Player player, @NotNull List<String> text) {
+        this.player = player;
+        this.text = text;
+    }
+
+    void onOpen(@NotNull Consumer<Location> onOpen) {
+        this.onOpen = onOpen;
+    }
+
+    @NotNull
+    public Menu reopenIfFail() {
+        this.reopenIfFail = true;
+        return this;
+    }
+
+    @NotNull
+    public Menu response(@NotNull BiPredicate<Player, List<String>> response) {
+        this.response = response;
+        return this;
+    }
+
+    @NotNull
+    public BiPredicate<Player, List<String>> response() {
+        return response;
+    }
+
+    public boolean isReopenIfFail() {
+        return reopenIfFail;
+    }
+
+    public void open() {
+        final Location location = player.getLocation().clone().subtract(0, 5, 0);
+
+        player.sendBlockChange(location, XMaterial.ACACIA_WALL_SIGN.parseMaterial().createBlockData());
+
+        final Object blockPosition = Sign.blockPosition.create(
+            null,
+            location.getBlockX(),
+            location.getBlockY(),
+            location.getBlockZ()
+        );
+        final Object openSignPacket = Sign.createPacket(OPEN_PACKET, blockPosition);
+
+        if (openSignPacket == null) {
+            return;
+        }
+
+        final NBTCompound signNBT = new EmptyNBTOf().nbt();
+
+        IntStream.range(0, SIGN_LINES).forEach(line ->
+            signNBT.setString("Text" + (line + 1), text.size() > line
+                ? String.format(NBT_FORMAT, new Colored(text.get(line)).value())
+                : "WW"
+            )
+        );
+        signNBT.setInt("x", (Integer) Sign.blockPositionX.of(blockPosition).get(0));
+        signNBT.setInt("y", (Integer) Sign.blockPositionY.of(blockPosition).get(0));
+        signNBT.setInt("z", (Integer) Sign.blockPositionZ.of(blockPosition).get(0));
+        signNBT.setString("id", NBT_BLOCK_ID);
+
+        final Object signDataPacket = Sign.createPacket(
+            ENTITY_DATA_PACKET,
+            blockPosition,
+            ACTION_INDEX,
+            signNBT.value()
+        );
+
+        if (signDataPacket == null) {
+            return;
+        }
+
+        Sign.sendPacket(player, signDataPacket);
+        Sign.sendPacket(player, openSignPacket);
+        onOpen.accept(location);
+    }
+
+}
--- src/main/java/io/github/portlek/sign/Sign.java
+++ src/main/java/io/github/portlek/sign/Sign.java
@@ -1,7 +1,140 @@
 package io.github.portlek.sign;
 
+import io.github.portlek.packetlisten.Packets;
+import io.github.portlek.reflection.RefClass;
+import io.github.portlek.reflection.RefConstructed;
+import io.github.portlek.reflection.RefField;
+import io.github.portlek.reflection.RefMethod;
+import io.github.portlek.reflection.clazz.ClassOf;
+import io.github.portlek.versionmatched.Version;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.cactoos.iterable.Mapped;
+import org.cactoos.list.ListOf;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public final class Sign {
 
+    private static final String UPDATE_PACKET = "PacketPlayInUpdateSign";
 
+    private static final Packets PACKETS = new Packets();
+
+    private final Map<Player, Menu> inputReceivers = new HashMap<>();
+
+    private final Map<Player, Location> signLocations = new HashMap<>();
+
+    @NotNull
+    private final Plugin plugin;
+
+    public Sign(@NotNull Plugin plugin) {
+        this.plugin = plugin;
+        listen();
+    }
+
+    @NotNull
+    public Menu newMenu(@NotNull Player player, @NotNull List<String> text) {
+        final Menu menu = new Menu(player, text);
+
+        menu.onOpen(location -> {
+            signLocations.put(player, location);
+            inputReceivers.putIfAbsent(player, menu);
+        });
+
+        return menu;
+    }
+
+    private void listen() {
+        PACKETS.prepareFor(plugin);
+        PACKETS.registerWrite(UPDATE_PACKET, event -> {
+            final Player player = event.getPlayer();
+            final Object packet = event.getPacket();
+            final List<String> input = new ListOf<>(
+                new Mapped<>(
+                    object -> (String) chatComponentGetText.of(object).call(""),
+                    updateSignFieldB.of(packet).get(packet)
+                )
+            );
+            final Menu menu = inputReceivers.remove(player);
+            final Location location = signLocations.remove(player);
+
+            if (menu == null || location == null) {
+                return;
+            }
+
+            event.setCancelled(true);
+
+            final boolean success = menu.response().test(player, input);
+
+            if (!success && menu.isReopenIfFail()) {
+                Bukkit.getScheduler().runTaskLater(plugin, menu::open, 2L);
+            }
+
+            player.sendBlockChange(location, Material.AIR.createBlockData());
+        });
+    }
+
+    private static final Version VERSION = new Version();
+
+    private static RefMethod getHandle;
+    private static RefField playerConnection;
+    private static RefField networkManager;
+    private static RefMethod sendPacket;
+    private static RefField updateSignFieldB;
+    private static RefMethod chatComponentGetText;
+    static RefConstructed blockPosition;
+    static RefField blockPositionX;
+    static RefField blockPositionY;
+    static RefField blockPositionZ;
+
+    static {
+        final String nms = "net.minecraft.server.v";
+
+        try {
+            getHandle = new ClassOf(nms + VERSION.raw() + ".entity.CraftPlayer").getMethod("getHandle");
+            playerConnection = new ClassOf(nms + VERSION.raw() + ".EntityPlayer").getField("playerConnection");
+            networkManager = new ClassOf(nms + VERSION.raw() + ".PlayerConnection").getField("networkManager");
+            final RefClass packetClass = new ClassOf(nms + Version.class + ".Packet");
+            sendPacket = new ClassOf(nms + VERSION.raw() + ".PlayerConnection")
+                .getMethod("sendPacket", packetClass.getRealClass());
+            updateSignFieldB = new ClassOf(nms + VERSION.raw() + UPDATE_PACKET).getField("b");
+            chatComponentGetText = new ClassOf(nms + VERSION.raw() + "IChatBaseComponent")
+                .getMethod("getText");
+            blockPosition = new ClassOf(nms + VERSION.raw() + ".BlockPosition")
+                .getConstructor(int.class, int.class, int.class);
+            final RefClass baseBlockPosition = new ClassOf(nms + VERSION.raw() + ".BaseBlockPosition");
+            blockPositionX = baseBlockPosition.getField("getX");
+            blockPositionY = baseBlockPosition.getField("getY");
+            blockPositionZ = baseBlockPosition.getField("getZ");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    static void sendPacket(@NotNull Player player, @NotNull Object packet) {
+        sendPacket.of(
+            networkManager.of(
+                playerConnection.of(
+                    getHandle.of(player).call(player)
+                ).get(player)
+            ).get(player)
+        ).call(player, packet);
+    }
+
+    @Nullable
+    static Object createPacket(@NotNull String className, @NotNull Object... parameters) {
+        try {
+            return new ClassOf(className).getConstructor(parameters).create(null, parameters);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
 
 }
